654c7a3e5a91e73496ad8d7512e04e9306241ba0
William Yager <will.yager@gmail.com>
2013-02-06 23:17:09 -0600

    Combined all features of palm-control and master
    Palm control and pointer control can now be done with the --finger
    (default) and --palm switches. The code has been restructured in a way
    that will make maintenance and algorithm upgrades much easier.

---
+++ PalmControl.py
@@ -0,0 +1,94 @@
+#William Yager
+#Leap Python mouse controller POC
+#This file is for palm-tilt and gesture-based control (--palm)
+
+import Leap
+import Geometry
+import math
+from MiscFunctions import *
+
+class Palm_Control_Listener(Leap.Listener): #The Listener that we attach to the controller. This listener is for palm tilt movement
+    def __init__(self, cursor):
+        super(Palm_Control_Listener, self).__init__() #Initialize like a normal listener
+        #Initialize a bunch of stuff specific to this implementation
+        self.screen = None
+        self.screen_resolution = (0,0)
+        self.cursor = cursor #The cursor object that lets us control mice cross-platform
+        self.gesture_debouncer = n_state_debouncer(5,3) #A signal debouncer that ensures a reliable, non-jumpy gesture detection
+        self.mouse_manager = mouse_manager() #This allows for cleaner mouse movement
+
+    def on_init(self, controller):
+        if controller.calibrated_screens.empty:
+            print "Calibrate your Leap screen feature"
+        self.screen = controller.calibrated_screens[0]
+        self.screen_resolution = (self.screen.width_pixels, self.screen.height_pixels)
+
+        print "Initialized"
+
+    def on_connect(self, controller):
+        print "Connected"
+
+    def on_disconnect(self, controller):
+        print "Disconnected"
+
+    def on_exit(self, controller):
+        print "Exited"
+
+    def on_frame(self, controller):
+        frame = controller.frame() #Grab the latest 3D data
+        if not frame.hands.empty: #Make sure we have some hands to work with
+            rightmost_hand = None #We always have at least one "right hand"
+            if len(frame.hands) < 2: #Just one hand
+                self.do_mouse_stuff(frame.hands[0]) #If there's only one hand, we assume it's to be used for mouse control
+            else: #Multiple hands. We have a right AND a left
+                rightmost_hand = max(frame.hands, key=lambda hand: hand.palm_position.x) #Get rightmost hand
+                leftmost_hand = min(frame.hands, key=lambda hand: hand.palm_position.x) #Get leftmost hand
+                self.do_gesture_recognition(leftmost_hand, rightmost_hand) #This will run with >1 hands in frame
+                                  
+    def do_mouse_stuff(self, hand): #Take a hand and use it as a mouse
+         hand_normal_direction = Geometry.to_vector(hand.palm_normal)
+         hand_direction = Geometry.to_vector(hand.direction)
+         roll = hand_normal_direction.roll()
+         pitch = hand_normal_direction.pitch()
+         mouse_velocity = self.convert_angles_to_mouse_velocity(roll, pitch)
+         movement = self.mouse_manager.add(mouse_velocity)
+         self.cursor.move(self.cursor.x + movement[0], self.cursor.y + movement[1])
+
+    #The gesture hand signals what action to do,
+    #The mouse hand gives extra data (if applicable)
+    #Like scroll speed/direction
+    def do_gesture_recognition(self, gesture_hand, mouse_hand):
+        if len(gesture_hand.fingers) == 2: #Two open fingers on gesture hand (scroll mode)
+            self.gesture_debouncer.signal(2) #Tell the debouncer we've seen this gesture
+        elif len(gesture_hand.fingers) == 1: #One open finger on gesture hand (click down)
+            self.gesture_debouncer.signal(1) 
+        else: #No open fingers or 3+ open fingers (click up/no action)
+            self.gesture_debouncer.signal(0)
+        #Now that we've told the debouncer what we *think* the current gesture is, we must act
+        #On what the debouncer thinks the gesture is
+        if self.gesture_debouncer.state == 2: #Scroll mode
+            y_scroll_amount = self.velocity_to_scroll_amount(mouse_hand.palm_velocity.y) #Mouse hand controls scroll amount
+            x_scroll_amount = self.velocity_to_scroll_amount(mouse_hand.palm_velocity.x)
+            self.cursor.scroll(x_scroll_amount, y_scroll_amount)
+        elif self.gesture_debouncer.state == 1: #Click/drag mode
+            if not self.cursor.left_button_pressed: self.cursor.click_down() #Click down (if needed)
+            self.do_mouse_stuff(mouse_hand) #We may want to click and drag
+        elif self.gesture_debouncer.state == 0: #Move cursor mode
+            if self.cursor.left_button_pressed: self.cursor.click_up() #Click up (if needed)
+            self.do_mouse_stuff(mouse_hand)   
+
+    def velocity_to_scroll_amount(self, velocity): #Converts a finger velocity to a scroll velocity
+        #The following algorithm was designed to reflect what I think is a comfortable
+        #Scrolling behavior.
+        vel = velocity #Save to a shorter variable
+        vel = vel + math.copysign(300, vel) #Add/subtract 300 to velocity
+        vel = vel / 150
+        vel = vel ** 3 #Cube vel
+        vel = vel / 8
+        vel = vel * -1 #Negate direction, depending on how you like to scroll
+        return vel
+
+    def convert_angles_to_mouse_velocity(self, roll, pitch): #Angles are in radians
+        x_movement = 5.0*math.copysign((4.0*math.sin(roll) + 2.0*roll)*math.sin(roll), roll)
+        y_movement = 5.0*math.copysign((4.0*math.sin(pitch) + 2.0*pitch)*math.sin(pitch), pitch)
+        return (x_movement, y_movement)
--- README.md
+++ README.md
@@ -2,7 +2,7 @@ README:
 
 This is my Proof-of-concept code for a Leap Motion-based mouse controller. It only works on OS X as-is, but if you wanted to rewrite Mouse.py for your OS, it should work fine. This was designed for API 0.7.1.
 
-USAGE:
+USAGE WITH FINGER MODE (python PyLeapMouse.py --finger) (default):
 Operation is as follows:
 1)Insert your hand into frame.
 2)The forwardmost finger that the program detects is the mouse finger. Where it points, the cursor goes.
@@ -10,6 +10,16 @@ Operation is as follows:
 4)Using two pointer fingers (e.g. index and middle) goes into scroll mode, which is not very intuitive but shows how it might work. The fingertips must be within a short distance of each other to activate scroll mode.
 
 (note) The Leap software at the moment (v0.7.1) is a little iffy about detecting thumbs, and it helps to keep your arm straight out in front of you. It takes a little practice to figure out how to hold your thumb correctly. You may want to un-comment some debug prints to help you figure out when Leap thinks you have your thumb out. Apparently 0.7.3 has better thumb detection, but it is not available at the time of writing.
+
+USAGE WITH PALM MODE (python PyLeapMouse.py --palm):
+Operation is as follows:
+One hand in frame: The tilt of this hand moves the mouse.
+Two hands in frame: Left hand controls action.
+    All fingers closed: Mouse movement with right hand tilt.
+    One finger open: Clicking. Left mouse button is down. Mouse movement with right hand tilt.
+    Two fingers open: Scrolling. Scrolling with right hand movement.
+
+This is a somewhat unintuitive method of operation, but I find that it gives exceptionally better control than the most obvious "point-at-screen" method of mouse control. With this two-handed tilt based mode, it is easy to hit and properly engage small buttons, scroll through webpages, etc.
 
 CONFIGURATION:
 1)Configure your Leap screen from the Leap menu.
@@ -19,3 +29,20 @@ CONFIGURATION:
 
 NOTES:
 I hacked this together in a few hours, so it's probably buggy and I doubt the design is anything to write home about. However, I tried to keep the code clean and readable. Let me know if you find any bugs. You can reach me at  will (dot) yager (at) gmail (dot) (what the gmail domain ends in). 
+
+The contents of the files are as follows:
+
+PyLeapMouse.py: The actual program
+
+FingerControl.py: Pointer-finger-control specific code
+PalmControl.py: Palm-tilt-control specific code
+
+Leap.py: Python-leap bindings (not my code)
+_LeapPython.so and libLeap.dylib: Leap libraries (not my code)
+
+Geometry.py: Geometric functions
+MiscFunctions.py: Things that aren't strictly geometry and aren't specific to any interface style
+
+Mouse.py: OS-Specific mouse commands (ideally the only OS-specific file). Subject to rapid change.
+
+README.md: You are here
