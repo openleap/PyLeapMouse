fceec4b60aeb7291a6aa6883a6e8a75535b99409
GCorbel <guirec.corbel@gmail.com>
2013-09-28 11:16:53 -0400

    make the code clearer

--- MotionControl.py
+++ MotionControl.py
@@ -6,14 +6,17 @@ class Motion_Control_Listener(Leap.Listener):  #The Listener that we attach to t
         super(Motion_Control_Listener, self).__init__()  #Initialize like a normal listener
 
     def on_init(self, controller):
-        self.init_list_of_commands()
+        self.read_config() #Read the config file
+        self.init_list_of_commands() #Initialize the list of recognized commands
 
         print "Initialized"
 
-    def init_list_of_commands(self):
+    def read_config(self):
         self.config = ConfigParser.ConfigParser()
         self.config.read("./commands.ini")
 
+    def init_list_of_commands(self):
+        #Initialize all commands an put it in an array
         self.commands = [
                 ScreentapCommand(),
                 SwiperightCommand(),
@@ -24,6 +27,7 @@ class Motion_Control_Listener(Leap.Listener):  #The Listener that we attach to t
         ]
 
     def on_connect(self, controller):
+        #Enable all gestures
         controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE);
         controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP);
         controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP);
@@ -40,38 +44,43 @@ class Motion_Control_Listener(Leap.Listener):  #The Listener that we attach to t
     def on_frame(self, controller):
         frame = controller.frame()  #Grab the latest 3D data
         if not frame.hands.empty:  #Make sure we have some hands to work with
-            for command in self.commands:
-                if(command.applicable(frame)):
-                    self.execute(frame, command.name)
+            for command in self.commands: #Loop all enabled commands
+                if(command.applicable(frame)): #If the motion associated to the command is triggered
+                    self.execute(frame, command.name) #Execute the command
 
     def execute(self, frame, command_name):
-        number_for_fingers = self.get_fingers_code(frame)
-        if(self.config.has_option(command_name, number_for_fingers)):
-            syscommand = self.config.get(command_name, number_for_fingers)
+        number_for_fingers = self.get_fingers_code(frame) #Get a text correspond to the number of fingers
+        if(self.config.has_option(command_name, number_for_fingers)): #If the command if finded in the config file
+            syscommand = self.config.get(command_name, number_for_fingers) #Prepare the command
             print(syscommand)
-            os.system(syscommand)
+            os.system(syscommand) #Execute the command
 
     def get_fingers_code(self, frame):
         return "%dfinger" % len(frame.fingers)
 
+
 class ScreentapCommand():
     def __init__(self):
         self.name = "screentap"
+        #The name of the command in the config file
 
+    #Return true if the command is applicable
     def applicable(self, frame):
         return(frame.gestures()[0].type == Leap.Gesture.TYPE_SCREEN_TAP)
 
 class KeytapCommand():
     def __init__(self):
-        self.name = "keytap"
+        self.name = "keytap" #The name of the command in the config file
 
+    #Return true if the command is applicable
     def applicable(self, frame):
         return(frame.gestures()[0].type == Leap.Gesture.TYPE_KEY_TAP)
 
 class SwiperightCommand():
     def __init__(self):
-        self.name = "swiperight"
+        self.name = "swiperight" #The name of the command in the config file
 
+    #Return true if the command is applicable
     def applicable(self, frame):
         swipe = SwipeGesture(frame.gestures()[0])
         return(swipe.state == Leap.Gesture.STATE_STOP
@@ -80,8 +89,9 @@ class SwiperightCommand():
 
 class SwipeleftCommand():
     def __init__(self):
-        self.name = "swipeleft"
+        self.name = "swipeleft" #The name of the command in the config file
 
+    #Return true if the command is applicable
     def applicable(self, frame):
         swipe = SwipeGesture(frame.gestures()[0])
         return(swipe.state == Leap.Gesture.STATE_STOP
@@ -90,8 +100,9 @@ class SwipeleftCommand():
 
 class ClockwiseCommand():
     def __init__(self):
-        self.name = "clockwise"
+        self.name = "clockwise" #The name of the command in the config file
 
+    #Return true if the command is applicable
     def applicable(self, frame):
         circle = CircleGesture(frame.gestures()[0])
         return(circle.state == Leap.Gesture.STATE_STOP and
@@ -100,8 +111,9 @@ class ClockwiseCommand():
 
 class CounterclockwiseCommand():
     def __init__(self):
-        self.name = "counterclockwise"
+        self.name = "counterclockwise" #The name of the command in the config file
 
+    #Return true if the command is applicable
     def applicable(self, frame):
         circle = CircleGesture(frame.gestures()[0])
         return(circle.state == Leap.Gesture.STATE_STOP and
